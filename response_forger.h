603de4c889463d4c769c4939cd5fa1385ccd0348
Theophane Joseph <theophane.joseph@epita.fr>
2020-09-26 18:24:54 +0200

    [FORGE] Forge functions done (not tested yet)

--- response_forger.h
+++ response_forger.h
@@ -9,3 +9,6 @@ void *response_forge(dns *ans); // struct -> packet (void*)
 int compute_strings_length(dns *ans);
 void *forge_header(dns *ans, dns_header *h);
 void *forge_quest(dns *ans, question *q);
+void *forge_answer(dns *ans, answer *a);
+void *forge_authority(dns *ans, answer *au);
+void *forge_additional(dns *ans, answer *ad);
