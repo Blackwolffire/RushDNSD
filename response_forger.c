e75ba5ece6fca3589fb4776749ca000f810186a4
Theophane Joseph <theophane.joseph@epita.fr>
2020-09-26 15:59:42 +0200

    [FORGE] Stopped to forging answer step (didn't test since the begin)

--- response_forger.c
+++ response_forger.c
@@ -2,14 +2,65 @@
 
 
 void *response_forge(dns *ans) {
-    void *res = malloc(sizeof(header + question + (answer * 3))); // FAUT CONNAITRE LA TAILLE DES CHAR*
+    int total_len = sizeof(dns_header);
+    total_len += compute_strings_length(ans);
+    total_len += 4 + (ans->head.ancount * 10) + (ans->head.nscount * 10) + (ans->head.arcount * 10);
+    void *res = malloc(total_len);
     void *p = forge_header(ans, res);
+    p = forge_quest(ans, p);
+
+    return res;
 }
 
-void *forge_header(dns *ans, header *h) {
-    *h = ans.head;
+int compute_strings_length(dns *ans) {
+    int quest_len = strlen(ans->quest->qname);
+
+    int answ_len = 0;
+    answer *answ_index = ans->answer;
+
+    for (int c = 0 ; c < ans->head.ancount ; c++) {
+        answ_len += strlen(answ_index->rname) + answ_index->rdlen;
+        answ_index++;
+    }
+
+    int auth_len = 0;
+    answer *auth_index = ans->authority;
+
+    for (int c = 0 ; c < ans->head.nscount ; c++) {
+        auth_len += strlen(auth_index->rname) + auth_index->rdlen;
+        auth_index++;
+    }
+
+    int addi_len = 0;
+    answer *addi_index = ans->additional;
+
+    for (int c = 0 ; c < ans->head.arcount ; c++) {
+        addi_len += strlen(addi_index->rname) + addi_index->rdlen;
+        addi_index++;
+    }
+
+    return quest_len + answ_len + auth_len + addi_len;
+}
+
+void *forge_header(dns *ans, dns_header *h) {
+    *h = ans->head;
     // little endian to big endian : htons
-    
+    h++;
+    return h;
 }
 
+void *forge_quest(dns *ans, question *q) {
+    strncpy((char*)q, (const char*)ans->quest->qname, strlen(ans->quest->qname));
+    q->qtype = ans->quest->qtype;
+    q->qclass = ans->quest->qclass;
+    q++;
 
+    return q;
+}
+
+/*
+void *forge_answer(dns *ans, answer *a) {
+    for (answ_index = 0 ; answ_index < answ_index->rdlen ; answ_index++) {
+        
+    }
+}*/
