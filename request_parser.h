18efa653be5c6771f2675472b3212a3eaa45dd96
corentin <cocogaigai7@gmail.com>
2020-09-26 15:29:23 +0200

    [PARSE] prepare merge

--- request_parser.h
+++ request_parser.h
@@ -3,6 +3,6 @@
 
 int size_name(char *request, int index, char *request_dns_name);
 
-dns *request_parser(void *request, size_t buf_size);
+dns *request_parser(void *request, int buffer_size);
 
 void printer(dns *dns_paquet);
