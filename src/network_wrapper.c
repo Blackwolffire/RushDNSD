e80dddd47c0edb861689eff9c69dc6ea0e2229e4
MLamanche <mlamanche@asith>
2020-09-26 13:24:44 -0500

    [NETWORK] implementing epoll events at initialisation

--- src/network_wrapper.c
+++ src/network_wrapper.c
@@ -1,6 +1,7 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <strings.h>
+#include <sys/epoll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -62,6 +63,41 @@ dns_engine* init_serv(dns_engine *engine, char *ip, uint16_t port)
     return NULL;
   }
 
+  int *epfds = malloc(sizeof(int) * nbip);
+  if (!epfds){
+    free(sockets);
+    engine->sockets= NULL;
+    free(engine->ip);
+    engine->ip = NULL;
+    engine->nbip = 0;
+    return NULL;
+  }
+
+  int *nbfd = malloc(sizeof(int) * nbip);
+  if (!nbfd){
+    free(epfds);
+    free(sockets);
+    engine->sockets= NULL;
+    free(engine->ip);
+    engine->ip = NULL;
+    engine->nbip = 0;
+    return NULL;
+  }
+
+  struct epoll_event *ep_events = malloc(sizeof(struct epoll_event) * nbip);
+  if (!ep_events){
+    free(nbfd);
+    free(epfds);
+    free(sockets);
+    engine->sockets= NULL;
+    free(engine->ip);
+    engine->ip = NULL;
+    engine->nbip = 0;
+    return NULL;
+  }
+
+  struct epoll_event **events = malloc(sizeof(struct epoll_event*) * nbip);
+
   int ipcheck, domain, error = 0;
   struct sockaddr_in addr;
   addr.sin_port = htons(port);
@@ -77,20 +113,55 @@ dns_engine* init_serv(dns_engine *engine, char *ip, uint16_t port)
       error = 1;
       break;
     }
+    if (listen(sockets[i], BACKLOG)){
+      error = 1;
+      break;
+    }
+    if ((epfds[i] = epoll_create(MAXEPOLLSIZE)) == -1){
+      error = 1;
+      break;
+    }
+    ep_events[i].events = EPOLLIN | EPOLLET;
+    ep_events[i].data.fd = sockets[i];
+    if (epoll_ctl(epfds[i], EPOLL_CTL_ADD, sockets[i], ep_events + i) < 0){
+      error = 1;
+      break;
+    }
+     events[i] = calloc(MAXEPOLLSIZE, sizeof(ep_events[i]));
+     if (!events[i]){
+       error = 1;
+       break;
+     }
+     nbfd[i] = 1;
   }
+
   if (error){
-    for (j=0; j <= i; ++j)
-      close(sockets[i]);
+    for (j=0; j <= i; ++j){
+      close(sockets[j]);
+      if (j < i)
+        free(events[j]);
+    }
+    free(events);
+    free(ep_events);
+    free(epfds);
+    free(nbfd);
     free(engine->sockets);
     engine->sockets = NULL;
     free(engine->ip);
     engine->ip = NULL;
     engine->nbip = 0;
+    return NULL;
   }
+  engine->nbfd = nbfd;
+  engine->epfds = epfds;
+  engine->ep_events = ep_events;
+  engine->events = events;
   engine->sockets = sockets;
   engine->port = port;
   return engine;
 }
+
+
 
 ssize_t dns_get(char **ptr, int socket)
 {
@@ -122,3 +193,5 @@ void close_serv(dns_engine *engine)
   engine->ip = NULL;
   engine->nbip = 0;
 }
+
+
