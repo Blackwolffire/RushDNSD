dc5e283b450d2f0cddf993ef6c5e7a4af187a2f0
titouan charlot--wauquier <titouan.charlot-wauquier@epita.fr>
2020-09-27 03:56:06 +0200

    [CATCH] added collector_exit() to inthandler, removed main

--- src/catch/catch.c
+++ src/catch/catch.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 
 #include "catch.h"
+#include "../collector/collector.h"
 
 // All the functions called when ctrl-c is caught go here
 static void inthandler(){
     // Functions run on interrupt go between these 2 comments
-    printf("Caught ctrl-c, exiting\n");
+    printf("Caught SIGINT, exiting ...\n");
+    collector_exit();
     // Functions run on interrupt go between these 2 comments
 
     exit(0);
@@ -16,11 +18,4 @@ static void inthandler(){
 // Catches ctrl-c input and calls functions to exit gracefully
 void catch(){
     signal(SIGINT, inthandler);
-}
-
-int main(int argc, char **argv){
-    catch();
-    char *s = malloc(64);
-    while (1);
-    return 0;
 }
