5d739bd9771890e53d2adf2c831306d41890a3b0
Thibaultt <thibault.lhuillier@epita.fr>
2020-09-27 08:26:31 +0200

    [PULL][master]

--- Makefile
+++ Makefile
@@ -10,12 +10,12 @@ VPATH= src/
 VPATH+= src/request_to_response/
 VPATH+= src/collector/ src/catch
 
-EXEC = server_dns
+EXEC = main #server_dns
 
 # FILES
-SRC= server_dns.c
+SRC= main.c #server_dns.c
 SRC+= analyser.c request_checker.c request_processer.c find_zone.c
-SRC+= server_dns.c dns_runner.c network_wrapper.c
+SRC+= dns_runner.c network_wrapper.c
 SRC+= dns_engine.c my_free.c my_libc.c
 SRC+= response_forger.c
 SRC+= request_parser.c
---
+++ src/main.c
@@ -0,0 +1,64 @@
+#include "request_parser.h"
+#include "response_forger.h"
+#include <stdlib.h>
+
+int main ()
+{
+	dns *dns_test = malloc(sizeof(struct dns));
+	dns_test->quest = malloc(sizeof(struct question));
+	dns_test->answer = malloc(sizeof(struct answer));
+	dns_test->authority = malloc(sizeof(struct answer));
+	dns_test->additional = malloc(sizeof(struct answer));
+	//head
+	dns_test->head.id = 1582;
+	dns_test->head.flags = 34816;
+	dns_test->head.qdcount = 1;
+	dns_test->head.ancount = 1;
+	dns_test->head.nscount = 1;
+	dns_test->head.arcount = 1;
+	//quest
+	dns_test->quest->qname = malloc(15);
+	dns_test->quest->qname = "www.google.com";
+	dns_test->quest->qtype = 1;
+	dns_test->quest->qclass = 1;
+	//answer
+	dns_test->answer->rname = malloc(15);
+	dns_test->answer->rname = "www.google.com";
+	dns_test->answer->rtype = 1;
+	dns_test->answer->rclass = 1;
+	dns_test->answer->ttl = 3;
+	dns_test->answer->rdlen = 12;
+	dns_test->answer->rdata = malloc(13);
+	dns_test->answer->rdata = "205.35.65.25";
+	//authority
+	dns_test->authority->rname = malloc(15);
+	dns_test->authority->rname = "www.google.com";
+	dns_test->authority->rtype = 1;
+	dns_test->authority->rclass = 1;
+	dns_test->authority->ttl = 3;
+	dns_test->authority->rdlen = 12;
+	dns_test->authority->rdata = malloc(13);
+	dns_test->authority->rdata = "205.35.65.25";
+	//additional
+	dns_test->additional->rname = malloc(15);
+	dns_test->additional->rname = "www.google.com";
+	dns_test->additional->rtype = 1;
+	dns_test->additional->rclass = 1;
+	dns_test->additional->ttl = 3;
+	dns_test->additional->rdlen = 12;
+	dns_test->additional->rdata = malloc(13);
+	dns_test->additional->rdata = "205.35.65.25";
+	size_t mdr = 0;
+
+	dns_engine *engine = dns_init(filename,port, "127.0.0.1");
+
+	dns *response = 
+
+
+	void *paquet_test = response_forge(dns_test, &mdr);
+	dns * res = request_parser(paquet_test,50);
+	
+	
+
+	return 0;
+}
